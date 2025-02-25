2d47f814aacdbd8ad3c4291a0ee5fa03db41d471
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-02-25 16:54:29 +0100

    feat: add print "Fizz" if multiple of 3

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ for (let i = 1; i <= 100; i++){
 // se un numero e multiplo sia di 3 che di 5 stampa FizzBuzz
     if (i % 3 === 0 && i % 5 === 0){
         console.log("FizzBuzz");
-// altrimenti stampa solo il numero
+// altrimenti se è un multiplo solo di 3 stampa Fizz
+    }else if (i % 3 === 0){
+        console.log("Fizz")
+// altrimenti se è un multiplo di 5 stampa Buzz       
     }else{
         console.log(i)
     }
